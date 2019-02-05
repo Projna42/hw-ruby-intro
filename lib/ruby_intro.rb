2ebe78fab077f0f0eaa798651d150dbfa2e1c13f
Projna42 <42851229+Projna42@users.noreply.github.com>
2019-02-04 21:35:37 -0600

    Homework 1
    Homework 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,30 +3,30 @@
 # Part 1
 
 def sum(array)
-  # YOUR CODE HERE
-  sum = 0
-  array.each{|x| sum += x}
-  return sum
+  # this method sums the elements of an array
+  sum = 0 #initializing
+  array.each{|x| sum += x} #adding the elements
+  return sum #returns summation, if array is empty, returns zero.
 end
 
 def max_2_sum(array)
-  # YOUR CODE HERE
-  sum = 0
-  array.sort!
-  if array.length != 0
+  # This method sums the 2 largest elements of an array
+  sum = 0 #initializing
+  array.sort!  #sorting in ascending order
+  if array.length != 0 #checking empty array
     if array.length>1
-      sum += array[-1]
-      sum += array[-2]
+      sum += array[-1] #the largest element
+      sum += array[-2] #the second largest element 
     else
-      sum +=array[-1]
+      sum +=array[-1] #incase array has 1 element
     end
   end
   return sum
 end
 
 def sum_to_n?(arr, n)
-  # YOUR CODE HERE
-  flag =0
+  # Checks whether summation of two numbers of an array equals the given number
+  flag =0 #when this becomes 1, we have such two numbers in the array
   for i in 0...arr.length
     if i != arr.length-1
       for j in i+1...arr.length
@@ -47,14 +47,13 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  # returns 'Hello, name'
   return "Hello, "<< name
   
 end
 
 def starts_with_consonant?(s)
-  # YOUR CODE HERE
-  #s.downcase!
+  # Finds whether the given string starts with consonant
   if s.length !=0
     if "AEIOUaeiou?<>',?[]}{=-)(*&^%$#`~{}0123456789".include? s[0]
       return false
@@ -68,14 +67,14 @@ def starts_with_consonant?(s)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  # checks if the string is binary number divided by 4
   if s.length == 0
     return false
   else
-    if s =~ /[^10]/
+    if s =~ /[^10]/ #checks if the string has charachters other than '10'
       return false
       
-    elsif s.to_i(2)%4 != 0 
+    elsif s.to_i(2)%4 != 0 #checks if the binary number is dividable by 4
       return false
     else
       return true
@@ -111,7 +110,7 @@ class BookInStock
     return @price
   end
   
-  def price_as_string
+  def price_as_string #pring price of the book to the two decimal point
     return "$%.2f" % @price
   end
   
